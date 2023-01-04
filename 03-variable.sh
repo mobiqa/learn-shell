9a7dbbfc4f46f7b1163cbd66c86499de84866475
venkat <venkat.mobiqa@gmail.com>
2023-01-04 09:32:56 +0530

    shell scripting topicss

---
+++ 03-variable.sh
@@ -0,0 +1,11 @@
+# if we assign a name to a set of data then it is called variable
+
+trainer=raghu
+student=venkat
+batchno=70
+class=devops
+
+echo Trainer Name - $trainer
+echo Student Name - $student
+echo Batch Number - $batchno
+echo Class Name - $class
