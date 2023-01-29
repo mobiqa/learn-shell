dd1a6537c39a27c15259c2c70d66b24d0ef1be89
venkat <venkat.mobiqa@gmail.com>
2023-01-29 20:38:27 +0530

    updating ./learn-shell

--- 02-print.sh
+++ 02-print.sh
@@ -9,18 +9,22 @@ echo "***hello world***"
 echo "hello venkat"
 
 #color code syntax : echo -e "\e[COLmMESSAGE\e[0m"
- RED   -31 - echo -e "\e[31mMESSAGE\e[0m"
- GREEN  -32 - echo -e "\e[32mMESSAGE\e[0m"
- YELLOW -33 - echo -e "\e[33mMESSAGE\e[0m"
- BLUE  -34 - echo -e "\e[34mMESSAGE\e[0m"
- MAGNETA -35 - echo -e "\e[35mMESSAGE\e[0m"
- CYAN -36 - echo -e "\e[36mMESSAGE\e[0m"
+#0m is reset the color
+# \e is a special character
+#COLm is color code
+# RED   -31 - echo -e "\e[31mMESSAGE\e[0m"
+# GREEN  -32 - echo -e "\e[32mMESSAGE\e[0m"
+# YELLOW -33 - echo -e "\e[33mMESSAGE\e[0m"
+# BLUE  -34 - echo -e "\e[34mMESSAGE\e[0m"
+# MAGNETA -35 - echo -e "\e[35mMESSAGE\e[0m"
+# CYAN -36 - echo -e "\e[36mMESSAGE\e[0m"
 #
-#echo -e "\e[31mMESSAGE\e[0m"
-#echo -e "\e[32mMESSAGE\e[0m"
-#echo -e "\e[33mMESSAGE\e[0m"
-#echo -e "\e[34mMESSAGE\e[0m"
-#echo -e "\e[35mMESSAGE\e[0m"
-#echo -e "\e[36mMESSAGE\e[0m"
-#
+
+echo -e "\e[31mMESSAGE\e[0m"
+echo -e "\e[32mMESSAGE\e[0m"
+echo -e "\e[33mMESSAGE\e[0m"
+echo -e "\e[34mMESSAGE\e[0m"
+echo -e "\e[35mMESSAGE\e[0m"
+echo -e "\e[36mMESSAGE\e[0m"
+
 
